97d1ca7a5e4478b6cf57acf24d265afcafe47c63
Ngesa.dev <ngesa.dev@gmail.com>
2025-06-14 23:21:25 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={ngesa_logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-extrabold cursor-pointer flex '>
-            Ngesa &nbsp;
+            Ngesa Kevin &nbsp;
             <span className='sm:block hidden'> | Practical Devs</span>
           </p>
         </Link>
